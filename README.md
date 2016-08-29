ef9c250a8a0fa2ea5f258c6a8c93ad9b1c4a8349
jinified <jinified@gmail.com>
2016-08-29 19:31:05 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Beach Volleyball Match Analysis
+A python application that analyzes beach volleyball matches to extract important information
+for tactical planning
+
+# Requirements
+1. Track all players movement from a top-down view
+2. Track the ball
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='volleyup',
+    version=0.1,
+    url='http://github.com/jinified/volleyup/',
+    author='Jin',
+    author_email='jinified@gmail.com',
+    description='Beach Volleyball Match Analysis',
+    license='Apache Software License',
+    keywords='tracking detection volleyball'
+    install_requires=['OpenCV>=3.0',
+                      ],
+    packages=['volleyup'],
+)
