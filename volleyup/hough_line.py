eea5d0436cf09a15d00ff8d705cf1b9851135b6b
Wei min Lee <a0108393@nus.edu.sg>
2016-10-24 12:58:00 +0800

    apply mask to detect court only

--- volleyup/hough_line.py
+++ volleyup/hough_line.py
@@ -29,25 +29,38 @@ class TranslationStitcher():
         for index, next_img in enumerate(self.imgs[1:]):
             ii += 1
             img = panorama_img
-            gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-            edges = cv2.Canny(gray,50,200,apertureSize = 3)
+
+            ''' roi mask to keep only court + players '''
+            img_width, img_height, img_channel = img.shape
+            x = 0
+            y = 150
+            w = img_height - x
+            h = img_width - y
+            masked_img = np.zeros(img.shape,np.uint8)
+            masked_img[y:y+h,x:x+w] = img[y:y+h,x:x+w]
+            
+            # img = cv2.GaussianBlur(img,(3,3),0)
+            gray = cv2.cvtColor(masked_img,cv2.COLOR_BGR2GRAY)
+            edges = cv2.Canny(gray,15,100,apertureSize = 3)
             ''' canny edge parameter test'''            
             
-            plt.subplot(121),plt.imshow(img[...,::-1],cmap = 'gray')
-            plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+            plt.subplot(121),plt.imshow(masked_img[...,::-1],cmap = 'gray')
+            plt.title('Masked Image'), plt.xticks([]), plt.yticks([])
             plt.subplot(122),plt.imshow(edges,cmap = 'gray')
             plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
 
             #plt.show()
             ''' canny edge detector displayed above '''
 
-            minLineLength = 10
+            minLineLength = 23
             #minLineLength = img.shape[1]-300
-            #maxLineGap = 10
+            maxLineGap = 10
             #lines = cv2.HoughLinesP(edges,1,np.pi/180,100,minLineLength,maxLineGap)
             #lines = cv2.HoughLines(edges,1,np.pi/180,0)
             #lines = cv2.HoughLines(edges,0.01,np.pi/720,100)
-            lines = cv2.HoughLinesP(image=edges,rho=0.02,theta=np.pi/360, threshold=1,lines=np.array([]), minLineLength=minLineLength,maxLineGap=10)
+            lines = cv2.HoughLinesP(image=edges,rho=0.02,theta=np.pi/360,
+                                    threshold=1,lines=np.array([]),
+                                    minLineLength=minLineLength,maxLineGap=maxLineGap)
 
             a,b,c = lines.shape
             for i in range(a):
