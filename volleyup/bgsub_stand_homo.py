6b33969efc845a006a9b08e99aa81dd4709850c0
Esyir <klim314@gmail.com>
2016-11-12 14:41:00 +0800

    ballfinder

--- volleyup/bgsub_stand_homo.py
+++ volleyup/bgsub_stand_homo.py
@@ -1,8 +1,24 @@
 import cv2
 import numpy as np
 from histogram import CourtFinder
+import os
 
-class LK_homo_bg:
+def subtract(original, deductible):
+    return np.uint8((original - np.int32(deductible)).clip(min = 0))
+
+def er_filter(image):
+    kernel = np.ones((3,3), np.uint8)
+    image = cv2.erode(image, kernel, iterations = 1)
+    image = cv2.dilate(image, kernel, iterations = 1)
+    return image
+
+def gaussian_noise_filter(image):
+    blur = cv2.GaussianBlur(image, (5,5), 0 )
+    thresh = cv2.threshold(blur, 33, 255, cv2.THRESH_BINARY)
+    return thresh[1]
+
+
+class LK_homo_tracker:
     """ Optical Flow tracking using Lucas-Kanade method based on sample/python/lk_track.py """
     def __init__(self):
         # Config
@@ -57,95 +73,219 @@ class LK_homo_bg:
 
         return (np.int32([tr[-1] for tr in self.tracks]).reshape(-1, 2), homography)
 
+class HomoBG:
+    """
+    Background subtractor
+    Uses median and homology to perform background subtraction
+    window_size: integer. Size of window to keep track of for background model calculation
+    """
+    def __init__(self, window_size = 5):
+        self.tracker = LK_homo_tracker()
+        self.court_finder = CourtFinder()
+        self.window_size =5
+
+    def run_test(self, video_path):
+        video = cv2.VideoCapture(video_path)
+        backgrounds_gray = []
+        acounter = 0
+
+        while True:
+            acounter += 1
+            ret, frame = video.read()
+            if not ret:
+                break
+            cols, rows, *_ = frame.shape
+            frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            # bgmed_gray = np.zeros((rows, cols))
+
+            mask =  255 - self.court_finder.process_frame(frame)
+            pts, homography = self.tracker.track_frame(frame, mask)
+            if acounter > self.window_size:
+                for i in range(-self.window_size, 0):
+                    backgrounds_gray[i] = cv2.warpPerspective(backgrounds_gray[i], homography, (rows, cols))
+                bgmed_gray = np.median(backgrounds_gray[-self.window_size:], axis = 0)
+            backgrounds_gray.append(frame_gray)
+
+            cv2.imshow("original", frame)
+            if acounter > self.window_size:
+                cv2.imshow("subtracted_-2", subtract(frame_gray, backgrounds_gray[-2]))
+                cv2.imshow("subtracted_med", subtract(frame_gray, bgmed_gray))
+            k = cv2.waitKey(10)
+            if k == 27:
+                print("Terminated by user")
+                exit()
+
+    def run(self, video_path, filter_funct = lambda x: x, startfrom = 0):
+        video = cv2.VideoCapture(video_path)
+        backgrounds_gray = []
+        acounter = 0
+
+        while True:
+            acounter += 1
+            
+            ret, frame = video.read()
+            if acounter < startfrom:
+                continue
+            if not ret:
+                break
+            cols, rows, *_ = frame.shape
+            frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            # bgmed_gray = np.zeros((rows, cols))
+
+            mask =  255 - self.court_finder.process_frame(frame)
+            pts, homography = self.tracker.track_frame(frame, mask)
+            if acounter - startfrom > self.window_size:
+                for i in range(-self.window_size, 0):
+                    backgrounds_gray[i] = cv2.warpPerspective(backgrounds_gray[i], homography, (rows, cols))
+                bgmed_gray = np.median(backgrounds_gray[-self.window_size:], axis = 0)
+            backgrounds_gray.append(frame_gray)
+
+            if acounter - startfrom > self.window_size:
+                yield (filter_funct(subtract(frame_gray, bgmed_gray)), frame_gray, frame)
+
+    def run_frames(self, frames, filter_funct = lambda x: x, startfrom = 0):
+        backgrounds_gray = []
+
+        for counter, frame in enumerate(frames):
+            cols, rows, *_ = frame.shape
+            frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            mask = 255 - self.court_finder.process_frame(frame)
+            pts, homography = self.tracker.track_frame(frame, mask)
+            if counter - startfrom > self.window_size:
+                for i in range(-self.window_size, 0):
+                    backgrounds_gray[i] = cv2.warpPerspective(backgrounds_gray[i], homography, (rows, cols))
+                bgmed_gray = np.median(backgrounds_gray[-self.window_size:], axis = 0)
+            backgrounds_gray.append(frame_gray)
+            if counter - startfrom > self.window_size:
+                yield (filter_funct(subtract(frame_gray, bgmed_gray)), frame_gray, frame)
+
+    def init_frame(self):
+        self.backgrounds_gray = []
+        self.counter = 0
+
+    def run_frame(self, frame, filter_funct = lambda x: x):
+        cols, rows, *_ = frame.shape
+        frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        mask = 255 - self.court_finder.process_frame(frame)
+        pts, homography = self.tracker.track_frame(frame, mask)
+        if self.counter > self.window_size:
+            for i in range(-self.window_size, 0):
+                self.backgrounds_gray[i] = cv2.warpPerspective(self.backgrounds_gray[i], homography, (rows, cols))
+            bgmed_gray = np.median(self.backgrounds_gray[-self.window_size:], axis = 0)
+        self.backgrounds_gray.append(frame_gray)
+        if self.counter > self.window_size:
+            return (filter_funct(subtract(frame_gray, bgmed_gray)), frame_gray, frame)
+        return None
+
+class BallFinder:
+    def __init__(self, filter_funct = er_filter, window_size = 5):
+        self.filter_funct = filter_funct
+        self.bgsub = HomoBG(window_size = 5)
+    
+    def init_frame(self, frame):
+        self.bgsub.init_frame()
+
+    def run_frame(self, frame):
+        res = self.bgsub.run_frame(frame)
+        if not res:
+            return None
+        subbed, gray, frame = res
+
+        subbed[0:5, :] = 0
+        subbed[:, 628:632] = 0
+        subbed[:, 0:7] = 0
+        subbed[295:300, :] = 0
+        subbed[250:300, 532:632] = 0
+
+        circles = cv2.HoughCircles(subbed, cv2.HOUGH_GRADIENT, 1, 20, param1=100, param2 =10, minRadius =1, maxRadius = 7)
+        if circles is not None:
+            return circles[0]
+        else:
+            return 0
+
+
+
 
         
 
+if __name__ == "__main__":
+    a = HomoBG(window_size = 3)
+    #a.run_test()
+    startfrom = 0
+    video_path = "data/beachVolleyball3.mov"
+    output_dir = "output/tests/bv3"
+    tt_dir = "data/training_data/bgsub/bv3"
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    if not os.path.exists(tt_dir):
+        os.makedirs(tt_dir)
+
+    video = cv2.VideoCapture(video_path) 
+
+    for i in range(startfrom):
+        ret, frame = video.read()
+    f_video = cv2.VideoCapture(video_path)
+
+    #Initialize frames for testing
+    frames = []
+    while True:
+        ret, frame = f_video.read()
+        if not ret or frame == None:
+            break
+        frames.append(frame)
+
+    # frame
+    for num, (i, gframe, frame) in enumerate(a.run_frames(frames, filter_funct = er_filter, startfrom = startfrom)):
+    # mov
+    # for num, (i, gframe, frame) in enumerate(a.run(video_path, filter_funct = er_filter, startfrom = startfrom)):
+        oframe = frame.copy()
+        #handle the edges because of homology bs
+        i[0:5, :] = 0
+        i[:, 628:632] = 0
+        i[:, 0:7] = 0
+        i[295:300, :] = 0
 
 
+        frame[0:5, :] = 0
+        frame[:, 628:632] = 0
+        frame[:, 0:7] = 0
+        frame[295:300, :] = 0
 
-video = cv2.VideoCapture("data/beachVolleyball1.mov")
-ret, oframe = video.read()
-cf = CourtFinder()
-tracker = LK_homo_bg()
-acounter = 0
+        i[250:300, 532:632] = 0
+        frame[250:300, 532:632] = 0
 
-background = np.float32(oframe)
-background_gray = np.float32(cv2.cvtColor(oframe, cv2.COLOR_BGR2GRAY))
-backgrounds_gray = []
+        cv2.imshow("test", i)
+        circles = cv2.HoughCircles(i, cv2.HOUGH_GRADIENT, 1, 20, param1=100, param2 =10, minRadius =1, maxRadius = 7)
+        if circles is not None:
+            print(circles)
+            for (x, y, r) in circles[0]:
+                cv2.circle(frame, (x,y), r, (0, 0, 255), 2)
+#                cv2.circle(frame, (x,y), r, (0, 0, 255), 2)
+        cv2.imshow("draw", frame)
+        #Write outputs
+        # cv2.imwrite(os.path.join(output_dir, "{}.png".format(str(num).zfill(4))), frame)
 
-while True:
-    acounter += 1
-    ret, frame = video.read()
-    cols, rows, *_ = frame.shape
-    if not ret:
-        break
-    mask =  255 - cf.process_frame(frame)
+        #Write train/test data
+        try:
+            if circles is not None:
+                print(circles)
+                for (x, y, r) in circles[0]:
+                    image = oframe[y-50:y+50, x-50:x+50]
+                    image2 = oframe.copy()
+                    cv2.circle(image2, (x, y), 4, (0, 0, 255), 3)
+                    image2 = image2[y-50:y+50, x-50:x+50]
+                    cv2.imshow("image", image2)
+                    cv2.imwrite(os.path.join(tt_dir, "{}.png".format(str(num).zfill(4))), image)
+                    cv2.imwrite(os.path.join(tt_dir, "{}_drawn.png".format(str(num).zfill(4))), image2)
 
-
-
-    features = frame.copy()
-    pts, homography = tracker.track_frame(frame, mask)
-    # print(pts)
-    for (x, y) in pts:
-        cv2.circle(features, (x,y), 5, 0, -1)
-
-    print([homography])
-    background = cv2.warpPerspective(background, homography, (rows, cols) )
-    alpha = 1/(acounter +1)
-    subframe = np.float32(frame) *(1-alpha)
-    print(background.shape)
-    print(subframe.shape)
-    cv2.accumulateWeighted(subframe, background, alpha)
-    bg = cv2.convertScaleAbs(background)
-
-    print("GRAY")
-    background_gray = cv2.warpPerspective(background_gray, homography, (rows, cols) )
-    alpha = 1/(acounter +1)
-    subframe = np.float32(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)) *(1-alpha)
-    print(background_gray.shape)
-    print(subframe.shape)
-    print([subframe])
-    cv2.accumulateWeighted(subframe, background_gray, alpha)
-    bg_gray = cv2.convertScaleAbs(background_gray)
-    backgrounds_gray.append(bg_gray)
-    if acounter > 3:
-        for i in range(-3, 0):
-            backgrounds_gray[i] = cv2.warpPerspective(background_gray, homography, (rows, cols))
-        print(backgrounds_gray[-3:])
-        bgmed_gray = np.median(backgrounds_gray[-3:], axis = 0)
-        bgmean_gray = np.mean(backgrounds_gray[-3:], axis = 0)
-
-
-    else:
-        bgmed_gray = bg_gray
-        bgmean_gray = bg_gray
-
-
-
-    cv2.imshow("mask", mask)
-    cv2.imshow("features", features)
-    cv2.imshow("background", bg)
-    cv2.imshow("subtracted", frame - bg)
-    cv2.imshow("background_gray", bg_gray)
-    print("bgmed")
-    print(bgmed_gray)
-    cv2.imshow("background_gray_median", bgmed_gray)
-    cv2.imshow("subtracted", cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) - bg_gray)
-    cv2.imshow("subtracted_med", cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) - bgmed_gray)
-    print(homography)
-
-
-
-
+        except Exception as err:
+            print(err, x, y, frame.shape)
 
 
 
 
-
-
-    k = cv2.waitKey(10)
-    if k == 27:
-        print("Terminated by user")
-        exit()
-
-
+        k = cv2.waitKey(10)
+        if k == 27:
+            print("Terminated by user")
+            exit()
